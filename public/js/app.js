75e90b16afbcb18514b1f4f257991622dd499569
Babs Doodkorte <babsdoodkorte@gmail.com>
2023-01-31 16:01:29 +0000

    Implemented functioning menu button

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,57 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  const backgroundButton = document.getElementById("postlist-button");
-  backgroundButton.addEventListener("click", function () {
-    const postBackground = document.getElementsByClassName("individualpostlist");
-    for(let i = 0; i < postBackground.length; i++) {
-      postBackground[i].style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
-    };
+  // const backgroundButton = document.getElementById("postlist-button");
+  // backgroundButton.addEventListener("click", function () {
+  //   const postBackground = document.getElementsByClassName("individualpostlist");
+  //   for(let i = 0; i < postBackground.length; i++) {
+  //     postBackground[i].style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+  //   };
+  // })
+
+  const menuButton = document.getElementById("menu-button");
+
+  menuButton.addEventListener("click", function () {
+
+    const userHeader = document.getElementById("ourmenuoptions");
+    const userButton = document.getElementById("menu-button");
+
+    userHeader.classList.remove("menuoptionnone");
+    userHeader.classList.add("menuoptionblock");
+
+    userButton.style.display = "none";
   })
+
+  const closeButton = document.getElementById("close-button");
+  closeButton.addEventListener("click", function () {
+
+    const userHeader = document.getElementById("ourmenuoptions");
+    const userButton = document.getElementById("menu-button");
+
+    userHeader.classList.remove("menuoptionblock");
+    userHeader.classList.add("menuoptionnone");
+
+    userButton.style.display = "block";
+  })
+
+
+    /*
+    if (userHeader.classList.contains("menuoptionnone")) {
+      userHeader.classList.remove("menuoptionnone");
+      userHeader.classList.add("menuoptionblock");
+    } else if (userHeader.classList.contains("menuoptionblock")) {
+      userHeader.classList.remove("menuoptionblock");
+      userHeader.classList.add("menuoptionnone");
+    }
+    */
+
+
+    // class="menuoptionblock"
+
+    // if (userHeader.style.display === "block") {
+    //   userHeader.style.display = "none";
+    // } else {
+    //   userHeader.style.display = "block";
+    // }
+
+
 });
